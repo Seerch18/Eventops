0d26dc94ebd8faaf03231af5a0b8881ce48f877e
Sergio <sergioespejo02@gmail.com>
2022-06-14 02:06:29 +0200

    alert search menu

--- src/app/components/menus/menu-busqueda/menu-busqueda.component.ts
+++ src/app/components/menus/menu-busqueda/menu-busqueda.component.ts
@@ -1,27 +1,25 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import { EventoService } from '../../../services/events/evento.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Evento } from '../../../models/Evento';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
-import { CiudadService } from '../../../services/ciudad/ciudad.service';
-import { FiltrosService } from '../../../services/filtros/filtros.service';
-import { EtiquetaService } from '../../../services/etiquetas/etiqueta.service';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core'
+import { EventoService } from '../../../services/events/evento.service'
+import { ActivatedRoute, Router } from '@angular/router'
+import { Evento } from '../../../models/Evento'
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
+import { CiudadService } from '../../../services/ciudad/ciudad.service'
+import { FiltrosService } from '../../../services/filtros/filtros.service'
+import { EtiquetaService } from '../../../services/etiquetas/etiqueta.service'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { MatDialog } from '@angular/material/dialog'
 
 @Component({
   selector: 'app-menu-busqueda',
   templateUrl: './menu-busqueda.component.html',
-  styleUrls: ['./menu-busqueda.component.css'],
+  styleUrls: ['./menu-busqueda.component.css']
 })
 export class MenuBusquedaComponent implements OnInit {
-  public frmFiltro: FormGroup;
-  public cities: Array<any>;
-  public tags: Array<any>;
-  @Output() eventos = new EventEmitter<any>();
+  public frmFiltro: FormGroup
+  public cities: Array<any>
+  public tags: Array<any>
+  @Output() eventos = new EventEmitter<any>()
+  durationInSeconds = 3
 
   constructor(
     private frmBuilder: FormBuilder,
@@ -29,23 +27,25 @@ export class MenuBusquedaComponent implements OnInit {
     private params: ActivatedRoute,
     private ciudadService: CiudadService,
     private filtroService: FiltrosService,
-    private etiquetaService: EtiquetaService
+    private etiquetaService: EtiquetaService,
+    private _snackBar: MatSnackBar,
+    public dialog: MatDialog
   ) {
-    this.cities = [];
-    this.tags = [];
+    this.cities = []
+    this.tags = []
     this.frmFiltro = this.frmBuilder.group({
       ciudad: new FormControl(''),
       etiquetas: new FormControl(''),
       fechaInicio: new FormControl(''),
       fechaFin: new FormControl(''),
       horaInicio: new FormControl(''),
-      horaFin: new FormControl(''),
-    });
+      horaFin: new FormControl('')
+    })
   }
 
   ngOnInit(): void {
-    this.getCities();
-    this.getTags();
+    this.getCities()
+    this.getTags()
   }
 
   filtrarEventos() {
@@ -53,24 +53,39 @@ export class MenuBusquedaComponent implements OnInit {
     this.filtroService
       .getFilteredEvents(this.frmFiltro.value)
       .subscribe((resp) => {
-        console.log(resp);
-        this.eventos.emit(resp);
-      });
+        console.log(resp)
+        if (resp) {
+          this.openSnackBar('Se han encontrado eventos')
+        } else {
+          this.openSnackBar('No se ha encontrado ningún evento')
+        }
+        this.eventos.emit(resp)
+      })
   }
 
   getCities() {
     this.ciudadService.readCity().subscribe((resp) => {
       if (resp) {
-        this.cities = resp;
+        this.cities = resp
       }
-    });
+    })
   }
 
   getTags() {
     this.etiquetaService.readTags().subscribe((resp) => {
       if (resp) {
-        this.tags = resp;
+        this.tags = resp
       }
-    });
+    })
+  }
+
+  /**
+   * Muestra una alerta con el mensaje recibido por parámetro
+   * @param message
+   */
+  openSnackBar(message: any) {
+    this._snackBar.open(message, 'UNDO', {
+      duration: this.durationInSeconds * 1000
+    })
   }
 }
