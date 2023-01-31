e2a1dec4bcdb542a5cbd20003f791a923c236406
Sergio <80220462+Seerch18@users.noreply.github.com>
2023-01-31 13:51:40 +0100

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  API_URL: 'https://www.iestrassierra.net/alumnado/curso2122/DAW/daw2122a3',
-  HREF_BASE:
-    'https://www.iestrassierra.net/alumnado/curso2122/DAW/daw2122a3/proyecto'
-}
